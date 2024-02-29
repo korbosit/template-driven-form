aada0fe811cc3b09bded9c5606f8d5be0e3857ed
Vlaimir Korba <krissky1986@gmail.com>
2024-02-29 10:18:03 +0200

    #113 - introduction to Template Driven Form | Template Driven Form |

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'template-driven-form';
+
+  @ViewChild('registrationForm') form: NgForm;
+
+  OnFormSubmitted() {
+    // console.log('Form submitted');
+    console.log(this.form);
+  }
 }
