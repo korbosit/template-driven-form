ed4efe34e281235259aea64b1cb975f1c3072c8c
Vlaimir Korba <krissky1986@gmail.com>
2024-03-01 11:40:45 +0200

    #120 - setvalue and patchValue method | Template Driven Form |

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,7 @@ export class AppComponent {
 
   firstName: string = '';
   lastName: string = '';
+  dob: string = '';
   emailAddress: string = '';
 
   defaultCountry: string = 'Country';
@@ -44,5 +45,61 @@ export class AppComponent {
     console.log(this.form.value.email);
     console.log(this.form.value.address.country);
     console.log(this.form.value.address.city);
+  }
+
+  GenerateUserName() {
+    let username = '';
+
+    if (this.firstName.length >= 3) {
+      username += this.firstName.slice(0, 3);
+    } else {
+      username += this.firstName;
+    }
+    if (this.lastName.length >= 3) {
+      username += this.lastName.slice(0, 3);
+    } else {
+      username += this.lastName;
+    }
+
+    let datetime = new Date(this.dob);
+    username += datetime.getFullYear();
+
+    username = username.toLowerCase();
+
+    console.log(username);
+
+    // this.form.value.usename = username;
+    // console.log(this.form.value.usename);
+
+    // this.form.controls['username'].value = username;
+
+    // The setValue() method is used to update a FormContol, FormGroup or FormArray value. To the setValue() method, we pass an object to update the value of a FormContol, FormGroup or FormArray. The structure of that object must match the structure of FormContol, FormGroup or FormArray which we are trying to update.
+
+    // this.form.setValue({
+    //   firstname: this.form.value.firstname,
+    //   lastname: this.form.value.lastname,
+    //   email: this.form.value.email,
+    //   phone: this.form.value.phone,
+    //   dob: this.form.value.dob,
+    //   gender: this.form.value.gender,
+    //   username: username,
+    //   address: {
+    //     street1: this.form.value.address.street1,
+    //     street2: this.form.value.address.street2,
+    //     country: this.form.value.address.country,
+    //     city: this.form.value.address.city,
+    //     region: this.form.value.address.region,
+    //     postal: this.form.value.address.postal,
+    //   },
+    // });
+
+    // The patchValue() method is used to update only a subset of the element of FormGroup or FormArray value.It will only update the matching objects and ignores the rest.
+
+    this.form.form.patchValue({
+      username: username,
+      address: {
+        country: 'Japan',
+      },
+    });
   }
 }
